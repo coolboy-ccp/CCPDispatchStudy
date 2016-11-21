f4ca7266a3639e29eed491b0ec1970ecc7a4a58d
储诚鹏 <ccp@iosyidongdemacbook-pro.local>
2016-11-21 17:15:34 +0800

    Initial Commit

---
+++ CCPDispatchStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCPDispatchStudy
+//
+//  Created by 储诚鹏 on 16/11/21.
+//  Copyright © 2016年 RUIYI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CCPDispatchStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CCPDispatchStudy
+//
+//  Created by 储诚鹏 on 16/11/21.
+//  Copyright © 2016年 RUIYI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
